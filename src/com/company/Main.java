1735e22f2dbcedc6d0f70c9c3ded56141c44f037
ajnura <ainura.zhanabidinkyzy.ch@gmail.com>
2022-04-26 12:14:38 +0600

    testing

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+
+//        String[] attary = {"gulnaz", "guzi", "nura" , "guli" };
+//        for (String a : attary) {
+//            System.out.println("men seni jakshy korom " +  a );}
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
